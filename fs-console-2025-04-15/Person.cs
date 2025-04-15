f943a909f12bed8aeb30050ea51e2bdf0f24e09b
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-04-15 21:15:39 +0100

    Files for tonight

---
+++ fs-console-2025-04-15/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fs_console_2025_04_15
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
+
+// change name to FirstName, add a LastName and an Age property
+// then initialise them in program.cs
+// output to the screen
+
+
--- fs-console-2025-04-15/Program.cs
+++ fs-console-2025-04-15/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using fs_console_2025_04_15;
+
+//Person p1 = new Person();
+
+//p1.Name = "John Doe";
+//p1.FirstName = "John";
+//p1.LastName = "Doe";
+//p1.Age = 30;
+////output all the properties to the screen
+
+//Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
+
+
+List<string> rainbow = new List<string> { "red", "orange", "yellow", 
+    "green", "blue", "indigo", "violet" };
+
+Console.WriteLine("<ol>");
+foreach (var colour in rainbow)
+{
+    Console.WriteLine($"<li>{colour}</li>");
+}
+Console.WriteLine("</ol>");
+
+
+Console.WriteLine("<table>");
+foreach (var colour in rainbow)
+{
+    Console.WriteLine($"<tr><td>{colour}</td></tr>");
+}
+Console.WriteLine("</table>");
+
+
+List<string> superHeroes = new List<string> { "Iron Man", "Captain America", "Spider-Man", "Ant-Man" };
+
+
+Console.WriteLine("<ul>");
+foreach (var hero in superHeroes)
+{
+    Console.WriteLine($"<li>{hero}</li>");
+}
+Console.WriteLine("</ul>");
+
